eecde57341c0f29ca1536fbbbad4ce77db542263
Stew Eucen <stew.eucen@gmail.com>
2022-08-01 16:31:51 +0900

    Declare CustomerReferralComplexModel.

---
+++ sequelize/complexModels/CustomerReferralComplexModel.js
@@ -0,0 +1,12 @@
+// @ts-check
+'use strict'
+
+const {
+  CustomerReferralComplexModel: RenchanCustomerReferralComplexModel,
+} = require('@openreachtech/renchan').complexModels
+
+class CustomerReferralComplexModel extends RenchanCustomerReferralComplexModel {
+
+}
+
+module.exports = CustomerReferralComplexModel
