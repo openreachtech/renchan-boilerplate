be66d8fe84914e38b7804348d8a456dd74ceef8e
JYama321 <yamajiiii54@gmail.com>
2024-07-18 12:31:10 +0700

    fix models

--- sequelize/models/Customer.js
+++ sequelize/models/Customer.js
@@ -2,13 +2,28 @@
 'use strict'
 
 const {
-  Customer: RenchanCustomer,
-} = require('@openreachtech/renchan').models
+  RenchanModel,
+  ModelAttributeFactory,
+} = require('@openreachtech/renchan-sequelize')
 
 /**
  * Customer model.
  */
-class Customer extends RenchanCustomer {
+class Customer extends RenchanModel {
+  /** @inheritdoc */
+  static createAttributes (DataTypes) {
+    const factory = ModelAttributeFactory.create(DataTypes)
+
+    return {
+      ...factory.ID_BIGINT,
+
+      registeredAt: {
+        type: DataTypes.DATE(3),
+        allowNull: false,
+      },
+    }
+  }
+
   /** @inheritdoc */
   static associate () {
     super.associate?.()
--- sequelize/models/ReferralNode.js
+++ sequelize/models/ReferralNode.js
@@ -5,10 +5,37 @@ const {
   ReferralNode: RenchanReferralNode,
 } = require('@openreachtech/renchan').models
 
+const {
+  ModelAttributeFactory,
+} = require('@openreachtech/renchan-sequelize')
+
 /**
  * Referral node model.
  */
 class ReferralNode extends RenchanReferralNode {
+  /** @inheritdoc */
+  static createAttributes (DataTypes) {
+    const factory = ModelAttributeFactory.create(DataTypes)
+
+    return {
+      ...factory.ID_BIGINT,
+
+      // Foreign keys must start with upper case
+      CustomerId: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+      },
+      ffQueue: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+      },
+      ffDepth: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+      },
+    }
+  }
+  
   /** @inheritdoc */
   static associate () {
     super.associate?.()
