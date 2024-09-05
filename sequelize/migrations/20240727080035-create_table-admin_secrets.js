e25514093fef95d4429f9f7334f795999b63d614
JYama321 <yamajiiii54@gmail.com>
2024-09-05 18:49:07 +0700

    add admin related migrations

---
+++ sequelize/migrations/20240727080035-create_table-admin_secrets.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const {
+  MigrationAttributeFactory,
+} = require('@openreachtech/renchan').sequelize
+
+const TABLE_NAME = 'admin_secrets'
+const COLUMN_NAME = {
+  ADMIN_ID: 'admin_id',
+  EMAIL: 'email',
+}
+
+module.exports = {
+  async up (
+    queryInterface,
+    Sequelize
+  ) {
+    const factory = MigrationAttributeFactory.create(Sequelize)
+
+    return queryInterface.createTable(
+      TABLE_NAME,
+      {
+        ...factory.ID_BIGINT,
+
+        AdminId: {
+          type: Sequelize.BIGINT,
+          field: COLUMN_NAME.ADMIN_ID,
+          allowNull: false,
+        },
+        email: {
+          type: Sequelize.STRING(191),
+          field: COLUMN_NAME.EMAIL,
+          allowNull: false,
+        },
+
+        ...factory.TIMESTAMPS,
+      }
+    )
+      .then(
+        () => queryInterface.addIndex(
+          TABLE_NAME,
+          [COLUMN_NAME.ADMIN_ID],
+          {
+            name: [
+              TABLE_NAME,
+              COLUMN_NAME.ADMIN_ID,
+              'index',
+            ].join('_'),
+          }
+        )
+      )
+  },
+
+  async down (
+    queryInterface,
+    Sequelize
+  ) {
+    return queryInterface.dropTable(TABLE_NAME)
+  },
+}
