a4e9ad55b40c1824363b909e0ebdf73f7ae570dc
Stew Eucen <stew.eucen@gmail.com>
2024-09-20 23:43:16 +0900

    Turn on arrow-body-style ESLint rule

--- eslint.config.js
+++ eslint.config.js
@@ -22,7 +22,6 @@ module.exports = [
   // Turn off some rules temporary
   {
     rules: {
-      'arrow-body-style': 'off',
       'no-empty-function': 'off',
 
       '@stylistic/arrow-parens': 'off',
