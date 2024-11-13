9661d463ecc3e494801711c83e38921e4b92e1ed
Stew Eucen <stew.eucen@gmail.com>
2024-11-13 15:50:32 +0900

    Comment out all codes in server bootstrap

--- server/index.js
+++ server/index.js
@@ -1,33 +1,35 @@
-import activate from './sequelize/_.js'
+// import activate from './sequelize/_.js'
 
-import GraphqlServerBuilder from '../lib/server/graphql/GraphqlServerBuilder.js'
+// import GraphqlServerBuilder from '../lib/server/graphql/GraphqlServerBuilder.js'
 
-import CustomerGraphqlServerEngine from './server/graphql/CustomerGraphqlServerEngine.js'
-import AdminGraphqlServerEngine from './server/graphql/AdminGraphqlServerEngine.js'
+// import CustomerGraphqlServerEngine from './server/graphql/CustomerGraphqlServerEngine.js'
+// import AdminGraphqlServerEngine from './server/graphql/AdminGraphqlServerEngine.js'
 
-await activate()
+// await activate()
 
-GraphqlServerBuilder.createAsync({
-  Engine: CustomerGraphqlServerEngine,
-})
-  .then(builder =>
-    builder.buildHttpServer()
-      .listen(3900)
-  )
-
-GraphqlServerBuilder.createAsync({
-  Engine: AdminGraphqlServerEngine,
-})
-  .then(builder =>
-    builder.buildHttpServer()
-      .listen(5800)
-  )
-
-// FIXME: 🚨🚨🚨🚨 This is not working.
-// AppRestfulServer.create({
-//   pathPrefix: '/v1',
+// GraphqlServerBuilder.createAsync({
+//   Engine: CustomerGraphqlServerEngine,
 // })
-//   .runAsRestfulApi({
-//     message: 'Express RESTful Server Now Running On http://localhost:8001/',
-//     port: 8001,
-//   })
+//   .then(builder =>
+//     builder.buildHttpServer()
+//       .listen(3900)
+//   )
+
+// GraphqlServerBuilder.createAsync({
+//   Engine: AdminGraphqlServerEngine,
+// })
+//   .then(builder =>
+//     builder.buildHttpServer()
+//       .listen(5800)
+//   )
+
+// // FIXME: 🚨🚨🚨🚨 This is not working.
+// // AppRestfulServer.create({
+// //   pathPrefix: '/v1',
+// // })
+// //   .runAsRestfulApi({
+// //     message: 'Express RESTful Server Now Running On http://localhost:8001/',
+// //     port: 8001,
+// //   })
+
+console.log('It works! 🎉')
