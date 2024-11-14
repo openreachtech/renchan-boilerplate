4e5eebf3b1abe609eabaeec3e7ecc7c935234dc4
Stew Eucen <stew.eucen@gmail.com>
2024-11-14 20:41:08 +0900

    Fix test for AdminGraphqlServerEngine.get:config

--- tests/__tests__/server/graphql/AdminGraphqlServerEngine.js
+++ tests/__tests__/server/graphql/AdminGraphqlServerEngine.js
@@ -30,9 +30,9 @@ describe('AdminGraphqlServerEngine', () => {
       const expected = {
         graphqlEndpoint: '/graphql-admin',
         staticPath: rootPath.to('public/'),
-        schemaPath: rootPath.to('app/server/graphql/schemas/admin-subscription.graphql'),
-        actualResolversPath: rootPath.to('app/server/graphql/resolvers/admin/actual/'),
-        stubResolversPath: rootPath.to('app/server/graphql/resolvers/admin/stub/'),
+        schemaPath: rootPath.to('server/graphql/schemas/admin.graphql'),
+        actualResolversPath: rootPath.to('server/graphql/resolvers/admin/actual/'),
+        stubResolversPath: rootPath.to('server/graphql/resolvers/admin/stub/'),
         redisOptions: null,
       }
 
