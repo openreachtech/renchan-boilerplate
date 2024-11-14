ce0de5d2e6d41d8addc33227ddb6dd33112aa443
Stew Eucen <stew.eucen@gmail.com>
2024-11-14 20:37:33 +0900

    Add moduleNameMapper property to jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@ export default {
   setupFilesAfterEnv: [
     '@openreachtech/renchan-test-tools/lib/environment/setupAfterEnv.js',
   ],
+  moduleNameMapper: {
+    '^(@.*)$': '<rootDir>/node_modules/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^sequelize/(.*)$': '<rootDir>/node_modules/sequelize/$1',
+  },
   testPathIgnorePatterns: [
     '/node_modules/',
   ],
