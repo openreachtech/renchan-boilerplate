32c7500696e290f917746b2686d8bf1f45c95c93
Stew Eucen <stew.eucen@gmail.com>
2024-11-17 01:37:00 +0900

    Add saved_at column to admin_password_hashes

--- sequelize/models/AdminPasswordHash.js
+++ sequelize/models/AdminPasswordHash.js
@@ -19,6 +19,10 @@ export default class AdminPasswordHash extends RenchanModel {
         type: DataTypes.STRING(191),
         allowNull: false,
       },
+      savedAt: {
+        type: DataTypes.DATE(3),
+        allowNull: false,
+      },
     }
   }
 
