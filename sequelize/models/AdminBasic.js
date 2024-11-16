cb9fb4729978199b4da5397789ed77f8d41a308d
Stew Eucen <stew.eucen@gmail.com>
2024-11-17 08:32:48 +0900

    Declare AdminBasic model

---
+++ sequelize/models/AdminBasic.js
@@ -0,0 +1,89 @@
+import {
+  BackupMixinModel,
+  RenchanModel,
+  ModelAttributeFactory,
+} from '@openreachtech/renchan-sequelize'
+
+/**
+ * AdminBasic model.
+ */
+export default class AdminBasic extends RenchanModel {
+  /** @inheritdoc */
+  static createAttributes (DataTypes) {
+    const factory = ModelAttributeFactory.create(DataTypes)
+
+    return {
+      ...factory.ID_BIGINT,
+
+      AdminId: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+      },
+      username: {
+        type: DataTypes.STRING(191),
+        allowNull: false,
+      },
+      savedAt: {
+        type: DataTypes.DATE(3),
+        allowNull: false,
+      },
+    }
+  }
+
+  /** @inheritdoc */
+  static createOptions (sequelizeClient) {
+    return {
+      ...super.createOptions(sequelizeClient),
+    }
+  }
+
+  /** @inheritdoc */
+  static associate (models) {
+    super.associate?.()
+
+    this.belongsTo(this._.Admin)
+  }
+
+  /** @inheritdoc */
+  static defineScopes (Op) {
+    super.defineScopes?.(Op)
+    // 必要に応じてスコープを定義
+  }
+
+  /** @inheritdoc */
+  static setupHooks () {
+    super.setupHooks?.()
+    // 必要に応じてフックを設定
+  }
+
+  /** @inheritdoc */
+  static defineSubqueries () {
+    super.defineSubqueries?.()
+    // 必要に応じてサブクエリを定義
+  }
+
+  /** @override */
+  static get Mixins () {
+    return [
+      BackupMixinModel,
+    ]
+  }
+
+  /**
+   * get: Backup model for BackupMixinModel
+   *
+   * @returns {typeof import('./AdminBasicsBk')} - Backup model declaration
+   */
+  static get BackupModel () {
+    return this._.AdminBasicsBk
+  }
+}
+
+/**
+ * @typedef {AdminBasic & {
+ *   id: number
+ *   AdminId: number
+ *   username: string
+ *   savedAt: Date
+ * }} AdminBasicEntity
+ */
