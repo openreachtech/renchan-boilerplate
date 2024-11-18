f243d19b07e89aa5a1237a5e7ea552c3d28108bf
Stew Eucen <stew.eucen@gmail.com>
2024-11-18 09:39:27 +0900

    Define SignInMutationResolver#findPasswordHashByEmail()

--- server/graphql/resolvers/customer/actual/mutations/SignInMutationResolver.js
+++ server/graphql/resolvers/customer/actual/mutations/SignInMutationResolver.js
@@ -2,6 +2,10 @@ import {
   BaseMutationResolver,
 } from '@openreachtech/renchan'
 
+import Customer from '../../../../../../sequelize/models/Customer.js'
+import CustomerPasswordHash from '../../../../../../sequelize/models/CustomerPasswordHash.js'
+import CustomerSecret from '../../../../../../sequelize/models/CustomerSecret.js'
+
 export default class SignInMutationResolver extends BaseMutationResolver {
   /** @override */
   static get schema () {
@@ -18,5 +22,52 @@ export default class SignInMutationResolver extends BaseMutationResolver {
         inviteCode: 'actual0123',
       },
     }
+  }
+
+  /**
+   * Find password customer by email.
+   *
+   * @param {{
+   *   email: string
+   * }} params
+   * @returns {Promise<import('../../../../../../sequelize/models/CustomerPasswordHash.js').CustomerPasswordHashEntity | null>}
+   */
+  async findPasswordHashByEmail ({
+    email,
+  }) {
+    /**
+     * @type {CustomerSecret & {
+     *   Customer: Customer & {
+     *     CustomerPasswordHash: CustomerPasswordHash
+     *   }
+     * } | null}
+     */
+    const customerSecretEntity = /** @type {*} */ (
+      await CustomerSecret.findOne({
+        where: {
+          email,
+        },
+        include: [
+          {
+            model: Customer,
+            include: [
+              CustomerPasswordHash,
+            ],
+          },
+        ],
+      })
+    )
+
+    if (!customerSecretEntity) {
+      return null
+    }
+
+    const {
+      Customer: {
+        CustomerPasswordHash: passwordHashEntity,
+      },
+    } = customerSecretEntity
+
+    return /** @type {*} */ (passwordHashEntity)
   }
 }
