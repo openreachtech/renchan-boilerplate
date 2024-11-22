757293763fa4482413f35b7ab4c75167e4285ec4
Stew Eucen <stew.eucen@gmail.com>
2024-11-22 15:31:58 +0900

    Replace IncorrectSecretSignInGraphqlError to #errorHash.IncorrectSecret in SignInMutationResolver

--- server/graphql/resolvers/customer/actual/mutations/SignInMutationResolver.js
+++ server/graphql/resolvers/customer/actual/mutations/SignInMutationResolver.js
@@ -7,8 +7,6 @@ import CustomerPasswordHash from '../../../../../../sequelize/models/CustomerPas
 import CustomerSecret from '../../../../../../sequelize/models/CustomerSecret.js'
 import CustomerAccessToken from '../../../../../../sequelize/models/CustomerAccessToken.js'
 
-import InvalidInputSignInGraphqlError from '../../../../errors/IncorrectSecretSignInGraphqlError.js'
-
 export default class SignInMutationResolver extends BaseMutationResolver {
   /** @override */
   static get schema () {
@@ -39,7 +37,7 @@ export default class SignInMutationResolver extends BaseMutationResolver {
     })
 
     if (!passwordHashEntity) {
-      throw InvalidInputSignInGraphqlError.create()
+      throw this.errorHash.IncorrectSecret.create()
     }
 
     const isValidPassword = await passwordHashEntity.verifiesPassword({
@@ -47,7 +45,7 @@ export default class SignInMutationResolver extends BaseMutationResolver {
     })
 
     if (!isValidPassword) {
-      throw InvalidInputSignInGraphqlError.create()
+      throw this.errorHash.IncorrectSecret.create()
     }
 
     const accessTokenEntity = await this.saveAccessToken({
