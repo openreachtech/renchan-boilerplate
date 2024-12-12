177cbec20ffa084d834b0d5a9763ba582124e311
Stew Eucen <stew.eucen@gmail.com>
2024-12-12 20:23:16 +0900

    Change error code from 10.00.01 to 100.X000.001

--- server/graphql/BaseAppGraphqlServerEngine.js
+++ server/graphql/BaseAppGraphqlServerEngine.js
@@ -14,7 +14,7 @@ export default class BaseAppGraphqlServerEngine extends BaseGraphqlServerEngine 
   /** @override */
   static get standardErrorCodeHash () {
     return {
-      Unknown: '10.00.01',
+      Unknown: '100.X000.001',
       ConcreteMemberNotFound: '11.00.01',
       Unauthenticated: '12.00.01',
       Unauthorized: '12.00.02',
