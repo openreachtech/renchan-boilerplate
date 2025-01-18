702958ce16c05bce0f2c724bb1d0769148fc4d41
Stew Eucen <stew.eucen@gmail.com>
2025-01-18 11:44:02 +0900

    Migrate UploadImageMutationResolver from renchan-core

---
+++ server/graphql/resolvers/customer/actual/mutations/UploadImageMutationResolver.js
@@ -0,0 +1,66 @@
+import {
+  setTimeout as sleep,
+} from 'timers/promises'
+
+import {
+  BaseMutationResolver,
+} from '@openreachtech/renchan'
+
+/**
+ * Upload image mutation resolver.
+ */
+export default class UploadImageMutationResolver extends BaseMutationResolver {
+  /** @override */
+  static get schema () {
+    return 'uploadImage'
+  }
+
+  /** @override */
+  static get errorCodeHash () {
+    return {
+      ...super.errorCodeHash,
+    }
+  }
+
+  /** @override */
+  async resolve ({
+    variables: {
+      input: {
+        image,
+      },
+    },
+  }) {
+    await sleep(300)
+
+    const contentReader = await this.Ctor
+      .createAsyncFileContentReader({
+        upload: image,
+      })
+
+    return this.formatResponse({
+      contentReader,
+    })
+  }
+
+  /**
+   * Format response.
+   *
+   * @param {{
+   *   contentReader: import('@openreachtech/renchan/lib/tools/FileContentReader').default
+   * }} params - Parameters.
+   * @returns {{
+   *   filename: string
+   *   mimetype: string
+   *   encoding: string
+   * }} - Response.
+   */
+  formatResponse ({
+    contentReader,
+  }) {
+    return {
+      filename: contentReader.filename,
+      mimetype: contentReader.mimetype,
+      encoding: contentReader.encoding,
+    }
+  }
+}
