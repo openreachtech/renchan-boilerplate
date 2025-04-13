570eb9be453274a809eeb708ed22a6283e25a4aa
Stew Eucen <stew.eucen@gmail.com>
2025-04-13 10:19:05 +0900

    Rename table variable to cases in test for validate-model-definitions

--- tests/__tests__/sequelize/models/validate-model-definitions.js
+++ tests/__tests__/sequelize/models/validate-model-definitions.js
@@ -1,10 +1,10 @@
 describe('Model', () => {
-  const table = Object.values(globalThis.sequelizeActivator.modelHash)
+  const cases = Object.values(globalThis.sequelizeActivator.modelHash)
     .map(Model => ({
       Model,
     }))
 
-  test.each(table)('Model: $Model.name', async ({ Model }) => {
+  test.each(cases)('Model: $Model.name', async ({ Model }) => {
     await expect(Model.findOne())
       .resolves
       .not.toThrow()
