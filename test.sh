34b89d8a721ce15cbda4bdd40e8cbd3aed6b4653
Stew Eucen <stew.eucen@gmail.com>
2026-06-26 23:13:36 +0900

    Support error on db:teardown

--- test.sh
+++ test.sh
@@ -13,7 +13,7 @@ function jestCommand () {
 function setupStorage () {
   blockTitle 'setup db with master seeds.'
 
-  npm run db:teardown
+  npm run db:teardown || echo 'skip error on teardown'
   npm run db:setup
   npm run db:seed:master
 }
