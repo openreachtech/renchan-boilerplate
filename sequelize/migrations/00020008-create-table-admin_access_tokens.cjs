175429c1425e23ff819dfe9e863753a3e554b98b
hieuguyen-ort <hieu.nguyen.202510@openreach.tech>
2026-08-12 10:38:50 +0700

    Add nullable session_key column to customer and admin access-token tables

--- sequelize/migrations/00020008-create-table-admin_access_tokens.cjs
+++ sequelize/migrations/00020008-create-table-admin_access_tokens.cjs
@@ -6,6 +6,7 @@ const TABLE_NAME = 'admin_access_tokens'
 const COLUMN_NAME = {
   ADMIN_ID: 'admin_id',
   ACCESS_TOKEN: 'access_token',
+  SESSION_KEY: 'session_key',
   GENERATED_AT: 'generated_at',
   EXPIRED_AT: 'expired_at',
 }
@@ -29,6 +30,12 @@ module.exports = {
         type: Sequelize.STRING(191),
         field: COLUMN_NAME.ACCESS_TOKEN,
         allowNull: false,
+      },
+      sessionKey: {
+        type: Sequelize.STRING(191),
+        field: COLUMN_NAME.SESSION_KEY,
+        // TODO: tighten to allowNull: false once every access-token writer sets sessionKey (later PR).
+        allowNull: true,
       },
       generatedAt: {
         type: Sequelize.DATE(3),
@@ -59,6 +66,15 @@ module.exports = {
         name: [
           TABLE_NAME,
           COLUMN_NAME.ACCESS_TOKEN,
+          'index',
+        ].join('_'),
+      }),
+      queryInterface.addIndex(TABLE_NAME, [
+        COLUMN_NAME.SESSION_KEY,
+      ], {
+        name: [
+          TABLE_NAME,
+          COLUMN_NAME.SESSION_KEY,
           'index',
         ].join('_'),
       }),
