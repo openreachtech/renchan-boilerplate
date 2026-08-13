3fb6b0c2085ab1ae2c734fce2015869a972f4bf5
hieuguyen-ort <hieu.nguyen.202510@openreach.tech>
2026-08-13 15:01:52 +0700

    Start admin session data

--- types/graphql.d.ts
+++ types/graphql.d.ts
@@ -4,6 +4,10 @@ declare global {
   namespace graphql {
     // NOTE: Define resolver input/output interfaces here.
 
+    interface RenewAccessTokenResult {
+      accessToken: string
+    }
+
     interface SignOutResult {
       isSignedOut: boolean
     }
